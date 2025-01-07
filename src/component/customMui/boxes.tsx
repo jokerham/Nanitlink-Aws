73e9655d7814a4423bd3d854e3b0b34a01d35975
Dongkyun Ham <jokerham@gmail.com>
2025-01-08 08:32:40 +0900

    changed the file name to show that there are multiple components.

---
+++ src/component/customMui/boxes.tsx
@@ -0,0 +1,27 @@
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+
+export const HorizontalBox = styled(Box)(({ theme }) => ({
+  display: 'inline-flex',
+  alignItems: 'center',
+  gap: theme.spacing(1),
+}));
+
+export const RowBox = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'row',
+  gap: theme.spacing(1),
+}));
+
+export const ColumnBox = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: theme.spacing(1),
+}));
+
+export const TitleBox = styled(Box)(({ theme }) => ({
+  borderBottom: '2px solid #ddd',
+  marginBottom: '24px',
+  height: '34px',
+  width: '100%',
+}));
