5bd293c14c89a77f8bec631dab663ac4b00aff06
Dongkyun Ham <jokerham@gmail.com>
2025-02-17 08:17:48 +0900

    Removed field related theme (moved to formbuilder variant theme)

--- src/layout/admin/theme.ts
+++ src/layout/admin/theme.ts
@@ -92,54 +92,6 @@ const theme = createTheme({
         },
       },
     },
-    MuiTextField: {
-      defaultProps: {
-        size: "small",
-        variant: "outlined",
-        fullWidth: true,
-      },
-      styleOverrides: {
-        root: {
-          '& .MuiInputBase-multiline': {
-            paddingLeft: 0,
-            paddingRight: 0,
-          },
-          '& .MuiInputLabel-root': {
-            top: -6,
-            fontSize: '14px',
-            paddingLeft: 0,
-            paddingRight: 0,
-          },
-          '& .MuiInputLabel-shrink': {
-            top: 0,
-          },
-        },
-      },
-    },
-    MuiSelect: {
-      defaultProps: {
-        size: "small",
-        variant: "outlined",
-      },
-      styleOverrides: {
-        root: {
-          paddingTop: "2px",
-          paddingBottom: "1px",
-        },
-      },
-    },
-    MuiOutlinedInput: {
-      styleOverrides: {
-        input: {
-          color: '#555',
-          fontFamily: 'Arial, sans-serif',
-          fontSize: '14px', 
-          fontWeight: 400,
-          height: 26,
-          padding: "0px 16px 0px 16px",
-        },
-      }
-    },
     MuiMenuItem: {
       styleOverrides: {
         root: {
@@ -148,39 +100,6 @@ const theme = createTheme({
           paddingBottom: '2px',
         },
       }
-    },
-    MuiFormControl: {
-      styleOverrides: {
-        root: {
-          '& .MuiInputLabel-root': {
-            top: -12,
-            fontSize: '14px',
-            paddingLeft: 0,
-            paddingRight: 0,
-          },
-          '& .MuiInputLabel-shrink': {
-            top: 0,
-          },
-        },
-      },
-    },
-    MuiFormControlLabel: {
-      styleOverrides: {
-        root: {
-          maxHeight: '30px', // Set max height for the entire checkbox control
-        },
-        label: {
-          fontFamily: 'Arial, sans-serif',
-          fontSize: '13px', // Set label font size
-        },
-      },
-    },
-    MuiCheckbox: {
-      styleOverrides: {
-        root: {
-          height: '30px', // Set the height of the checkbox itself
-        },
-      },
     },
     MuiTable: {
       styleOverrides: {
