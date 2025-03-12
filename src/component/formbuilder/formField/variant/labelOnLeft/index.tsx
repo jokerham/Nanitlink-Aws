af6c67ba4c6dc5342d7a57c1ee2a43df865e632e
Dongkyun Ham <jokerham@gmail.com>
2025-03-13 07:11:13 +0900

    Provided different UI for Hidden case.

--- src/component/formbuilder/formField/variant/labelOnLeft/index.tsx
+++ src/component/formbuilder/formField/variant/labelOnLeft/index.tsx
@@ -1,4 +1,4 @@
-import { TFieldSetting } from "component/formbuilder/types";
+import { EFieldType, TFieldSetting } from "component/formbuilder/types";
 import { Box, Grid2, Typography } from "@mui/material";
 import { SmallSize } from "../smallSize";
 
@@ -22,19 +22,23 @@ const labelStyle = {
 export const LabelOnLeft = (props: TFieldSetting) => {
   const { label, ...fieldSetting} = props;
 
-  return (
-    <Grid2 container spacing={2} sx={containerStyle}>
-      <Grid2 size={2} sx={labelStyle}>
-        <Typography variant="h4">
-          {(fieldSetting.required ?? false) && (
-            <Box component="small" sx={{color: '#F00', mr: '4px'}}>*</Box>
-          )}
-          {label}
-        </Typography>
+  if (fieldSetting.type === EFieldType.Hidden) {
+    return <SmallSize {...fieldSetting as TFieldSetting}/>;
+  } else {
+    return (
+      <Grid2 container spacing={2} sx={containerStyle}>
+        <Grid2 size={2} sx={labelStyle}>
+          <Typography variant="h4">
+            {(fieldSetting.required ?? false) && (
+              <Box component="small" sx={{color: '#F00', mr: '4px'}}>*</Box>
+            )}
+            {label}
+          </Typography>
+        </Grid2>
+        <Grid2 size={10}>
+          <SmallSize {...fieldSetting as TFieldSetting}/>
+        </Grid2>
       </Grid2>
-      <Grid2 size={10}>
-        <SmallSize {...fieldSetting as TFieldSetting}/>
-      </Grid2>
-    </Grid2>
-  );
+    );
+  }
 }
