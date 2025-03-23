172951554daecfee0602a4a84d5ff7e74a8fa7d1
Dongkyun Ham <jokerham@gmail.com>
2025-03-23 09:26:48 +0900

    Created Loading Component

---
+++ src/component/commom/Loading.tsx
@@ -0,0 +1,36 @@
+import React, { Fragment } from 'react';
+import { Box, CircularProgress, createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  components: {
+    MuiCircularProgress: {
+      styleOverrides: {
+        root: {
+          width: "100px !important",
+          height: "100px !important",
+        },
+      },
+    },
+  },
+});
+
+interface ILoadingProps {
+  loading: boolean
+  children?: React.ReactNode
+}
+
+const Loading = ({loading, children}: ILoadingProps) => {
+  if (loading) {
+    return (
+      <ThemeProvider theme={theme}>
+        <Box display="flex" justifyContent="center" alignItems="center" height="100vh">
+          <CircularProgress />
+        </Box>
+      </ThemeProvider>
+    );
+  } else {
+    return <Fragment>{children}</Fragment>;
+  }
+}
+
+export default Loading;
