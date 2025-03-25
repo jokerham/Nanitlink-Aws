e2032456b997ce3078fd105c71259cb605203e14
Dongkyun Ham <jokerham@gmail.com>
2025-03-26 05:02:37 +0900

    created default create for board

---
+++ src/module/board/defaultCreate.ts
@@ -0,0 +1,30 @@
+import { Board } from "@/API";
+import { createBoard } from "@/graphql/mutations";
+import { generateClient, GraphQLResult } from "aws-amplify/api";
+
+interface ICreateDefaultParameter {
+  id: string,
+  name: string
+}
+
+export async function createDefault({id, name}: ICreateDefaultParameter): Promise<Board | null> {
+  const client = generateClient();
+
+  try {
+    const response = (await client.graphql({
+      query: createBoard,
+      variables: {
+        input: {
+          id,
+          name,
+        },
+      },
+      authMode: 'userPool'
+    })) as GraphQLResult<{ createBoard: Board }>;
+
+    return response.data?.createBoard || null;
+  } catch (error) {
+    console.error('Error creating board:', error);
+    return null;
+  }
+}
